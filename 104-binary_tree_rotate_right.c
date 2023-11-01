adc5ea59415413d07bfb79dac8e4a5f66bad3bb9
CarltonNcube <carltonncube@outlook.com>
2023-11-01 13:08:38 -0700

    a function that performs a right-rotation on a binary tree

--- 104-binary_tree_rotate_right.c
+++ 104-binary_tree_rotate_right.c
@@ -8,29 +8,27 @@
  */
 binary_tree_t *binary_tree_rotate_right(binary_tree_t *tree)
 {
-    if (tree == NULL || tree->left == NULL)
-        return (NULL);
+	binary_tree_t *pivot, *tmp;
 
-    binary_tree_t *pivot = tree->left;
-    binary_tree_t *tmp = pivot->right;
+	if (tree == NULL || tree->left == NULL)
+		return (NULL);
 
-    pivot->right = tree;
-    tree->left = tmp;
+	pivot = tree->left;
+	tmp = pivot->right;
+	pivot->right = tree;
+	tree->left = tmp;
+	if (tmp != NULL)
+		tmp->parent = tree;
+	tmp = tree->parent;
+	tree->parent = pivot;
+	pivot->parent = tmp;
+	if (tmp != NULL)
+	{
+		if (tmp->left == tree)
+			tmp->left = pivot;
+		else
+			tmp->right = pivot;
+	}
 
-    if (tmp != NULL)
-        tmp->parent = tree;
-
-    tmp = tree->parent;
-    tree->parent = pivot;
-    pivot->parent = tmp;
-
-    if (tmp != NULL)
-    {
-        if (tmp->left == tree)
-            tmp->left = pivot;
-        else
-            tmp->right = pivot;
-    }
-
-    return pivot;
+	return (pivot);
 }
